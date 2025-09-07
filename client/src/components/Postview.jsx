37849ecbcd9489aa2a96959bc0c347146fa42351
sudhanshu chaurasia <sudhiitg@gmail.com>
2025-09-07 16:59:53 +0530

    Added photo upload feature in post

---
+++ client/src/components/Postview.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Comment from './comment.jsx';
+export default function Postview() {
+  const { slug } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const res = await fetch(`/api/post/getPosts?slug=${slug}`);
+        const data = await res.json();
+
+        if (!res.ok) {
+          console.error(data.message);
+          return;
+        }
+
+        setPost(data.posts[0]);
+      } catch (error) {
+        console.error('Error fetching post:', error);
+      }
+    };
+
+    fetchPost();
+  }, [slug]);
+
+
+
+  
+  if (!post) {
+    return (
+      <div className="text-center text-gray-500 mt-10">
+        Loading post...
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-6xl mx-auto p-6  rounded-xl mt-6">
+      <h1 className="text-3xl font-bold mb-4 ">{post.title}</h1>
+      {post.image && (
+        <img
+          src={post.image}
+          alt="Blog"
+          className="w-full h-auto object-cover rounded-md mb-4"
+        />
+      )}
+      {/* <div className="leading-relaxed whitespace-pre-line">
+        {post.content}
+      </div> */}
+      <div
+  className="prose "
+  dangerouslySetInnerHTML={{ __html: post.content }}
+></div>
+{<Comment post={post}/>}
+    </div>
+    
+  );
+}
