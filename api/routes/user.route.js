cf4dd103b2321643b24d51df6badcd9c5724e1f0
sudhanshu chaurasia <sudhiitg@gmail.com>
2025-09-07 17:06:00 +0530

    post and user route added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import {test} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
-// import { signout } from '../controllers/auth.controller.js';
+import { signout } from '../controllers/auth.controller.js';
+import { deleteUser,updateUser} from '../controllers/user.controller.js';
 
 const router =express.Router();
 
 router.get('/test',test);
-// router.post('/update/:id',verifyToken, updateUser);
-// router.delete('/delete/:id',verifyToken,deleteUser);
+router.post('/update/:id',verifyToken, updateUser);
+router.delete('/delete/:id',verifyToken,deleteUser);
 
 // router.get('/listing/:id', verifyToken, getUserListings)
-// router.get('/:id', verifyToken, getUser)
+//  router.get('/:id', verifyToken, getUser)
 export default router ;
