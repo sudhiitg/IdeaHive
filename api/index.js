0eecdcebc032951ed9be0ef7528507d7ef774079
sudhanshu chaurasia <sudhiitg@gmail.com>
2025-09-07 20:37:55 +0530

    Final installation

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import postRouter from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
 // // import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
-// import path from 'path';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 const app =express();
 // app.use(express.json()); 
 app.use(cookieParser()); 
@@ -40,10 +40,10 @@ app.use("/api/post",postRouter);
 app.use("/api/comment",commentRouter);
 
 
-// app.use(express.static(path.join(__dirname,'/client/dist')));
-// app.get('*',(req,res)=>{
-//    res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
-// })
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+   res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
    const statusCode=err.statusCode || 500;
