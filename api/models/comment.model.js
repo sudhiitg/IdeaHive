11fa7e47adafd4654480584e39308c308741aaa9
sudhanshu chaurasia <sudhiitg@gmail.com>
2025-09-07 17:05:23 +0530

    User,Post and Comment Schema Added

---
+++ api/models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    postId:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    }
+    
+},{timestamps:true})
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
+
